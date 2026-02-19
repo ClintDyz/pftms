542481304b3cb6f59405b31b63cef9042b3e4d0e
dostcarmis <azraelbaleg@gmail.com>
2026-02-19 16:50:59 +0800

    Update the BURS ORS and DV header logo fixed the placement of the borders

--- app/Plugins/PDFGenerator/DocObligationRequestStatus.php
+++ app/Plugins/PDFGenerator/DocObligationRequestStatus.php
@@ -46,32 +46,30 @@ class DocObligationRequestStatus extends PDF {
         $this->AddPage();
 
         /* ------------------------------------- Start of Doc ------------------------------------- */
+//Title header with logo
+if ($data->ors->document_type == 'ors') {
+    $docSubject = "Obligation Request and Status";
+    $this->SetFont('helvetica', 'B', 15 + ($fontScale * 15));
+} else {
+    $docSubject = "Budget Utilization Request and Status";
+    $this->SetFont('helvetica', 'B', 14 + ($fontScale * 14));
+}
 
-        //Title header with logo
-        if ($data->ors->document_type == 'ors') {
-            $docSubject = "Obligation Request and Status";
-            $this->SetFont('helvetica', 'B', 15 + ($fontScale * 15));
-        } else {
-            $docSubject = "Budget Utilization Request and Status";
-            $this->SetFont('helvetica', 'B', 14 + ($fontScale * 14));
-        }
+$this->Cell($pageWidth * 0.5714, 8, strtoupper($docSubject), 'TLR', 0, 'C');
+$this->Cell(0, 8, '', 'TR');
+$this->Ln();
 
-        $this->Cell($pageWidth * 0.5714, 8, strtoupper($docSubject), 'TLR', 0, 'C');
-        $this->Cell(0, 8, '', 'TR');
-        $this->Ln();
+$xCoor = $this->getX();
+$yCoor = $this->getY();
 
+$arrContextOptions = [
+    "ssl" => [
+        "verify_peer" => false,
+        "verify_peer_name" => false,
+    ],
+];
 
-        $xCoor = $this->getX();
-        $yCoor = $this->getY();
-
-        $arrContextOptions = [
-            "ssl" => [
-                "verify_peer" => false,
-                "verify_peer_name" => false,
-            ],
-        ];
-
-//start
+// Load logo
 $img = file_get_contents(
     url('images/logo/dostlogoupdate.png'),
     false,
@@ -82,30 +80,101 @@ $img = file_get_contents(
 $startX = $this->GetX();
 
 // Define layout widths
-$logoAndTextWidth = $pageWidth * 0.57;  // Left side for logo (which includes text)
-$rightBoxWidth = $pageWidth * 0.43;     // Right side for Serial No. and Date
+$logoAndTextWidth = $pageWidth * 0.5714;   // Left side for logo
+$rightBoxWidth = $pageWidth * 0.4286;      // Right side for Serial No. and Date
 
-// Draw left cell with LEFT border only
-$this->Cell($logoAndTextWidth, 12, '', 'L', 0, 'L');
+// Draw left cell with LEFT and TOP borders
+$this->Cell($logoAndTextWidth, 6, '', 'LT', 0, 'L');
 
-// Insert the logo into the left cell area
-$this->Image('@' . $img, $startX + 3, $yCoor + 1, 100, 0, 'PNG');
+// Insert the logo
+$this->Image('@' . $img, $startX + 3, $yCoor, 100, 0, 'PNG');
 
 // Get the X position for the right section
 $rightStartX = $this->GetX();
 
-// Draw right section WITH LEFT and RIGHT borders
+// Draw right section - Serial No. line with TOP, LEFT, RIGHT borders
 $this->SetFont('helvetica', '', 9 + ($fontScale * 9));
+$this->Cell($rightBoxWidth, 6, 'Serial No.  : ' . $data->ors->serial_no, 'TLR', 2, 'L');
 
-// Serial No. line
-$this->Cell($rightBoxWidth, 6, 'Serial No.  : ' . $data->ors->serial_no, 'LR', 2, 'L');
+// Continue left logo area - second row with LEFT border only
+$this->SetX($startX);
+$this->Cell($logoAndTextWidth, 6, '', 'L', 0, 'L');
 
-// Date line - need to reset X to align properly
+// Date line - with LEFT and RIGHT borders
 $this->SetX($rightStartX);
 $this->Cell($rightBoxWidth, 6, 'Date          : ' . $orsDate, 'LR', 1, 'L');
 
-// Move to next row (Entity Name)
-$this->Ln(0);
+// Close the bottom borders
+$this->SetX($startX);
+$this->Cell($logoAndTextWidth, 0, '', 'LB', 0, 'L');
+$this->Cell($rightBoxWidth, 0, '', 'RB', 1, 'L');
+
+// Entity Name row
+$this->SetFont('helvetica','IB', 11 + ($fontScale * 11));
+$this->Cell($pageWidth * 0.5714, 6, 'Entity Name', 'LRB', 0, 'C');
+$this->SetFont('helvetica','IB', 10 + ($fontScale * 10));
+$this->Cell(0, 6, "Fund Cluster \t\t\t\t: ____________________", 'RB');
+$this->Ln();
+
+//         //Title header with logo
+//         if ($data->ors->document_type == 'ors') {
+//             $docSubject = "Obligation Request and Status";
+//             $this->SetFont('helvetica', 'B', 15 + ($fontScale * 15));
+//         } else {
+//             $docSubject = "Budget Utilization Request and Status";
+//             $this->SetFont('helvetica', 'B', 14 + ($fontScale * 14));
+//         }
+
+//         $this->Cell($pageWidth * 0.5714, 8, strtoupper($docSubject), 'TLR', 0, 'C');
+//         $this->Cell(0, 8, '', 'TR');
+//         $this->Ln();
+
+
+//         $xCoor = $this->getX();
+//         $yCoor = $this->getY();
+
+//         $arrContextOptions = [
+//             "ssl" => [
+//                 "verify_peer" => false,
+//                 "verify_peer_name" => false,
+//             ],
+//         ];
+
+// //start
+// $img = file_get_contents(
+//     url('images/logo/dostlogoupdate.png'),
+//     false,
+//     stream_context_create($arrContextOptions)
+// );
+
+// // Save the starting X position
+// $startX = $this->GetX();
+
+// // Define layout widths
+// $logoAndTextWidth = $pageWidth * 0.5714;  // Left side for logo (which includes text)
+// $rightBoxWidth = $pageWidth * 0.466667;     // Right side for Serial No. and Date
+
+// // Draw left cell with LEFT border only
+// $this->Cell($logoAndTextWidth, 12, '', 'L', 0, 'L');
+
+// // Insert the logo into the left cell area
+// $this->Image('@' . $img, $startX + 3, $yCoor + 1, 100, 0, 'PNG');
+
+// // Get the X position for the right section
+// $rightStartX = $this->GetX();
+
+// // Draw right section WITH LEFT and RIGHT borders
+// $this->SetFont('helvetica', '', 9 + ($fontScale * 9));
+
+// // Serial No. line
+// $this->Cell($rightBoxWidth, 6, 'Serial No.  : ' . $data->ors->serial_no, 'LR', 2, 'L');
+
+// // Date line - need to reset X to align properly
+// $this->SetX($rightStartX);
+// $this->Cell($rightBoxWidth, 6, 'Date          : ' . $orsDate, 'LR', 1, 'L');
+
+// // Move to next row (Entity Name)
+// $this->Ln(0);
 
         // $img = file_get_contents(url('images/logo/dostlogo_update.png'), false,
         //                         stream_context_create($arrContextOptions));
@@ -148,14 +217,14 @@ $this->Ln(0);
         // $this->Cell(0, 4, "Date \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t: " . $orsDate, 'R');
         // $this->Ln();
 
-        // This section closes the bottom border of the logo/Serial No. area
-$this->SetFont('helvetica','', 9 + ($fontScale * 9));
+//         // This section closes the bottom border of the logo/Serial No. area
+// $this->SetFont('helvetica','', 9 + ($fontScale * 9));
 
-// Left section - add BOTTOM border
-$this->Cell($pageWidth * 0.57, 4, '', 'LB', 0, 'L');
+// // Left section - add BOTTOM border
+// $this->Cell($pageWidth * 0.5714, 4, '', 'LB', 0, 'L');
 
-// Right section - add BOTTOM border
-$this->Cell($pageWidth * 0.43, 4, '', 'RB', 1, 'L');
+// // Right section - add BOTTOM border
+// $this->Cell($pageWidth * 0.466667, 4, '', 'RB', 1, 'L');
 
         // $this->SetFont('helvetica','', 9 + ($fontScale * 9));
         // $this->Cell($pageWidth * 0.10476,4,'','L');
@@ -164,16 +233,16 @@ $this->Cell($pageWidth * 0.43, 4, '', 'RB', 1, 'L');
         //     $this->SetTextColor(0, 0, 0);
         // }
 
-        $this->Cell($pageWidth * 0.466667,4,'','R');
-        $this->SetTextColor(0, 0, 0);
-        $this->Cell(0, 4, '', 'R');
-        $this->Ln();
+        // $this->Cell($pageWidth * 0.466667,4,'','R');
+        // $this->SetTextColor(0, 0, 0);
+        // $this->Cell(0, 4, '', 'R');
+        // $this->Ln();
 
-        $this->SetFont('helvetica','IB', 11 + ($fontScale * 11));
-        $this->Cell($pageWidth * 0.57143,6, 'Entity Name','LRB', 0, 'C');
-        $this->SetFont('helvetica','IB', 10 + ($fontScale * 10));
-        $this->Cell(0, 6, "Fund Cluster \t\t\t\t: ____________________", 'RB');
-        $this->Ln();
+        // $this->SetFont('helvetica','IB', 11 + ($fontScale * 11));
+        // $this->Cell($pageWidth * 0.57143,6, 'Entity Name','LRB', 0, 'C');
+        // $this->SetFont('helvetica','IB', 10 + ($fontScale * 10));
+        // $this->Cell(0, 6, "Fund Cluster \t\t\t\t: ____________________", 'RB');
+        // $this->Ln();
 
         //Header data
         $this->SetFont('helvetica', '', 9 + ($fontScale * 9));
