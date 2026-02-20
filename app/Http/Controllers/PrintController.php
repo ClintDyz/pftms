888a319035b93e393e0fbd80e149724c77293b28
dostcarmis <azraelbaleg@gmail.com>
2026-02-20 10:41:57 +0800

    Update the DV ORS BURS

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -3134,25 +3134,36 @@ class PrintController extends Controller
                         $dv->responsibility_center,
                         $mfoPAP, $amount];
 
-        $dataHeader = [
-            [
-                'aligns' => ['L', 'L', 'L', 'L'],
-                'widths' => [10.4762 * $multiplier, 38.095 * $multiplier,
-                             26.19 * $multiplier, 16.6684 * $multiplier],
-                'font-styles' => ['B', 'B', '', ''],
-                'type' => 'other',
-                'data' => [['Payee', $payee,
-                            'TIN/Employee No.:<br>_____________________________',
-                            'ORS/BURS No.:<br>'. ($dv->serial_no ?? 'N/A')]]
-            ], [
-                'col-span' => true,
-                'col-span-key' => ['0', '1-3'],
-                'aligns' => ['L', 'L', 'L', 'L'],
-                'widths' => [10.4762 * $multiplier, 80.9534 * $multiplier, '', ''],
-                'font-styles' => ['B', 'B', 'B', 'B'],
-                'type' => 'other',
-                'data' => [["Address", $dv->address]]
-            ]
+$dataHeader = [
+    [
+        'aligns' => ['L', 'L', 'L', 'L'],
+        'widths' => [10.4762 * $multiplier, 38.095 * $multiplier,
+                     26.19 * $multiplier, 16.6684 * $multiplier],
+        'font-styles' => ['B', '', '', ''],  // Changed first 'B' to '' for second element
+        'type' => 'other',
+        'data' => [['Payee', $payee,
+                    'TIN/Employee No.:',
+                    'ORS/BURS No.:']]  // Removed <br> tags
+    ],
+    [
+        'aligns' => ['L', 'L', 'L', 'L'],
+        'widths' => [10.4762 * $multiplier, 38.095 * $multiplier,
+                     26.19 * $multiplier, 16.6684 * $multiplier],
+        'font-styles' => ['', '', '', ''],
+        'type' => 'other',
+        'data' => [['', '',
+                    '_____________________________',
+                    $dv->serial_no ?? 'N/A']]  // Second row with underlines and value
+    ],
+    [
+        'col-span' => true,
+        'col-span-key' => ['0', '1-3'],
+        'aligns' => ['L', 'L', 'L', 'L'],
+        'widths' => [10.4762 * $multiplier, 80.9534 * $multiplier, '', ''],
+        'font-styles' => ['B', '', '', ''],  // Changed 'B' to '' for address value
+        'type' => 'other',
+        'data' => [["Address", $dv->address]]
+    ]
         ];
         $data = [
             [
